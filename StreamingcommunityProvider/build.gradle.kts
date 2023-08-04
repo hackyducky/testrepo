e5a32f48c882b5a25ab8bfe161ece06ea5fae54e
Dario Zarcone <dariozarcone@gmail.com>
2023-08-04 18:04:51 +0200

    Fix SC favicon

--- StreamingcommunityProvider/build.gradle.kts
+++ StreamingcommunityProvider/build.gradle.kts
@@ -22,5 +22,5 @@ cloudstream {
         "Movie",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=streamingcommunity.bet&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=streamingcommunity.gold&sz=%size%"
 }
